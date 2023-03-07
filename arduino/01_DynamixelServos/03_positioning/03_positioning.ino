8cbf52a82ad5e84de225a993fd1a25a7fecc2f65
Cristopher Rufasto <cristopher.rufasto.m@uni.pe>
2023-03-07 14:34:20 -0500

    Added Dynamixel Servos examples

---
+++ arduino/01_DynamixelServos/03_positioning/03_positioning.ino
@@ -0,0 +1,58 @@
+#include <Dynamixel2Arduino.h>
+
+// We're using OpenCR Board
+#define DXL_SERIAL   Serial3
+#define DEBUG_SERIAL Serial
+const int DXL_DIR_PIN = 84; // OpenCR Board's DIR PIN.
+
+#define N_DXL 2
+const uint8_t DXL_IDs[N_DXL] = {4, 8};
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+//This namespace is required to use Control table item names
+using namespace ControlTableItem;
+
+void setup() {
+  // Use UART port of DYNAMIXEL Shield to debug.
+  DEBUG_SERIAL.begin(115200);
+  while(!DEBUG_SERIAL);
+
+  dxl.begin(57600);
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  for(int i = 0; i < N_DXL; i++){
+    // Get DYNAMIXEL information
+    dxl.ping(DXL_IDs[i]);
+
+    // Configure mode in EEPROM area
+    dxl.torqueOff(DXL_IDs[i]);
+    dxl.setOperatingMode(DXL_IDs[i], OP_POSITION);
+    dxl.torqueOn(DXL_IDs[i]);
+
+    // Limit the max velocity in Position Control MOde. Use 0 for Max Speed
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_IDs[i], 1000);
+  }
+}
+
+#define N_goals 3
+const float goals[N_goals] = {200, 250, 130};
+
+void loop() {
+  for(int i = 0; i < N_goals; i++){
+    for(int j = 0; j < N_DXL; j++){
+      const float goal = goals[i];
+      float position = 0.0;
+
+      dxl.setGoalPosition(DXL_IDs[j], goal, UNIT_DEGREE);
+
+      while(abs(goal - position) > 2.0){
+        position = dxl.getPresentPosition(DXL_IDs[j], UNIT_DEGREE);
+        DEBUG_SERIAL.print("Present_Position(degree) : ");
+        DEBUG_SERIAL.println(position);
+      }
+
+      delay(1000);      
+    }
+  }
+}
